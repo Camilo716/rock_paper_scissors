16f6a84a02606f64d972954bb5775580fb0d7004
camilogs <camilo.gonzalez.col@gmail.com>
2023-06-18 19:53:18 -0500

    Implement choosen move logic for human player

--- RockPaperScissors/Players/HumanPlayer.py
+++ RockPaperScissors/Players/HumanPlayer.py
@@ -1,4 +1,5 @@
 from Players.IPlayer import IPlayer
+from RockPaperScissors.logic.Moves import Moves
 from UI.IRockPaperScsUI import IRockPaperScsUI;
 
 class HumanPlayer(IPlayer):
@@ -6,8 +7,17 @@ class HumanPlayer(IPlayer):
         self.UI = UI
 
     def choose_move(self):
+        valid_moves = list(move.value for move in Moves)
+
         while True:
-            self.UI.read_player_move()
+            choosen_move = self.UI.read_player_move()
+            
+            if choosen_move in valid_moves:
+                self.UI.show_game_msj("Invalid Movement")
+                continue
+
+            return choosen_move
+            
             
              
             

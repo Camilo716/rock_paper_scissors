eec31d449cf9741beb40f193f89672e7b99b1056
camilogs <camilo.gonzalez.col@gmail.com>
2023-06-18 19:19:39 -0500

    Make explicit abstract class implementations

--- RockPaperScissors/logic/RockPaperScissorsGame.py
+++ RockPaperScissors/logic/RockPaperScissorsGame.py
@@ -1,8 +1,10 @@
+from Players.IPlayer import IPlayer
+from UI.IRockPaperScsUI import IRockPaperScsUI
 import logic.Moves as Moves
 
 class RockPaperScissorsGame():
     
-    def __init__(self, UI, human_player, pc_player):
+    def __init__(self, UI:IRockPaperScsUI, human_player:IPlayer, pc_player:IPlayer, rounds:IPlayer):
         self.UI = UI
         self.human_player = human_player
         self.pc_player = pc_player

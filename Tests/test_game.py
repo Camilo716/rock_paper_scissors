8d9d92d6fd9c8170609aac7d8d82bf9458bd637c
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-04 18:17:45 -0500

    Refactor: rename test class and method

--- Tests/test_game.py
+++ Tests/test_game.py
@@ -5,15 +5,14 @@ from Tests.Stubs.PlayerStubPaper import PlayerStubPaper
 from Tests.Stubs.PlayerStubRock import PlayerStubRock
 from Tests.Stubs.PlayerStubScissors import PlayerStubScissors
 
-class test_game(unittest.TestCase):
+class test_winner_handler(unittest.TestCase):
 
-    def test_compare_moves(self): 
-
+    def test_find_winner(self): 
         stub_paper = PlayerStubPaper("StubPaper")
         stub_rock = PlayerStubRock("StubRock")
         stub_scissors = PlayerStubScissors("StubScissors")
-        
-        WinnerGameCases = [
+         
+        WinnerGameCases = [ 
             (stub_paper, stub_rock, stub_paper),
             (stub_paper, stub_scissors, stub_scissors),
             (stub_scissors, stub_rock, stub_rock),
